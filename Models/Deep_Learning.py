e65a5f11f38d92730d907b24ea9edb3109e11fc8
DataHiveMind <77356196+DataHiveMind@users.noreply.github.com>
2024-12-26 16:02:21 +0000

    keras_update

--- Models/Deep_Learning.py
+++ Models/Deep_Learning.py
@@ -2,10 +2,27 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
-def build_lstm_model(input_shape):
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+def basic_keras_model(data, test_data, predictors, target):
+    model = Sequential()
+    n_cols = data.shape[1]
+    model.add(Dense(5, activition = 'relu', input_shape = (n_cols, )))
+    model.add(Dense(5, activition = 'relu'))
+    model.add(Dense(1))
+
+    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+    model.fit(predictors, target)
+    predictions = model.predict(test_data)
+    return predictions
+
+def build_lstm_model(input_shape, units):
     """Builds an LSTM model.
 
     Args:
+        units: number of units for model
         input_shape: Shape of the input data.
 
     Returns:
@@ -13,8 +30,8 @@ def build_lstm_model(input_shape):
     """
 
     model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=input_shape))
-    model.add(LSTM(units=50))
+    model.add(LSTM(units=units, return_sequences=True, input_shape=input_shape))
+    model.add(LSTM(units=units))
     model.add(Dense(units=1))
     model.compile(optimizer='adam', loss='mean_squared_error')
     return model
